5ba3e2a18e34bb5eb10e45a00a3108b66a3624bc
C. Titus Brown <titus@idyll.org>
2016-02-02 09:06:24 -0800

    update to run coverage

--- tc_tests/test_everything.sh
+++ tc_tests/test_everything.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python_interpreter=python2.5
+python_interpreter=/Users/t/dev/quast/env/bin/coverage
 
 sh ../clean.sh
 
 for f in test_*.py; do
-    $python_interpreter "$f";
+    $python_interpreter run "$f";
     if [ ! $? -eq 0 ]; then exit 1; fi;
     echo;
     echo;
